a62e772c2e257b1d6568fd19dd5994f3628aae6e
Rodrigo Azevedo <rodrigo4zevedo@gmail.com>
2025-06-23 18:37:36 +0200

    add more scraps

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import os
 import time
+import re
 
 import requests
 from bs4 import BeautifulSoup
@@ -8,6 +9,27 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+
+leagues = {
+    # "data/clubs/uruguay-primera-division": "/uruguay/liga-auf-uruguaya",
+    # "data/clubs/argentina-primera-division": "/argentina/torneo-betano",
+    # "data/clubs/brazil-brasileirao-serie-a": "/brazil/serie-a-betano",
+    # "data/clubs/brazil-copa-do-brasil": "/brazil/copa-betano-do-brasil",
+    # "data/clubs/chile-primera-division": "/chile/liga-de-primera",
+    # "data/clubs/bolivia-primera-division": "/bolivia/division-profesional",
+    # "data/clubs/colombia-primera-a": "/colombia/primera-a",
+    # "data/clubs/ecuador-liga-pro": "/ecuador/liga-pro",
+    # "data/clubs/paraguay-copa-de-primera": "/paraguay/copa-de-primera",
+    # "data/clubs/peru-liga-1": "/peru/liga-1",
+    # "data/clubs/venezuela-liga-futve": "/venezuela/liga-futve",
+    # "data/clubs/mexico-liga-mx": "/mexico/liga-mx",
+    "data/clubs/south-america-copa-libertadores": "/south-america/copa-libertadores",
+    "data/clubs/south-america-copa-sudamericana": "/south-america/copa-sudamericana",
+    "data/clubs/north-central-america-concacaf-champions-cup": "/north-central-america/concacaf-champions-cup",
+    "data/clubs/usa-mls": "/usa/mls",
+}
+
+MAIN_URL = "https://www.flashscore.com"
 
 
 def download_logo(src, team_name):
@@ -48,6 +70,52 @@ def extract_results_from_html(html_content):
         download_logo(away_logo, away_team)
         results.append(",".join([date, home_team, away_team, home_score, away_score]))
     return results
+
+
+def get_archive(url):
+    try:
+        print("Setting options...")
+        # Set up the Chrome browser
+        options = Options()
+        ua = UserAgent()
+        user_agent = ua.random
+
+        options.add_argument(f"user-agent={user_agent}")
+        # options.add_argument("--headless")
+        options.add_argument("--start-maximized")
+
+        # adding argument to disable the AutomationControlled flag
+        options.add_argument("--disable-blink-features=AutomationControlled")
+
+        # exclude the collection of enable-automation switches
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
+        # turn-off userAutomationExtension
+        options.add_experimental_option("useAutomationExtension", False)
+
+        driver = webdriver.Chrome(options=options)
+
+        # changing the property of the navigator value for webdriver to undefined
+        driver.execute_script(
+            "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
+        )
+
+        print("Opening Flashscore ...")
+        # Open the page
+        driver.get(f"{MAIN_URL}/football{url}/archive/")
+        time.sleep(2)
+
+        source = driver.page_source
+        pattern = rf"(\/football{url}\-.*\/)"
+        matches = re.findall(pattern, source)
+        return matches
+
+    except TimeoutException:
+        print("Not able to parse.")
+    except Exception as e:
+        print(str(e))
+    finally:
+        driver.quit()
 
 
 def extract_results(url, filename):
@@ -113,15 +181,16 @@ def extract_results(url, filename):
 
 
 if __name__ == "__main__":
-    folder = "data/clubs/uruguay-primera-division"
-    os.makedirs(folder, exist_ok=True)
-    for year in range(2006, 2016):
-        filename = os.path.join(folder, f"{year}-{year+1}.csv")
-        if not os.path.exists(filename):
-            url = (
-                f"https://www.flashscore.com/football/uruguay/liga-auf-uruguaya-{year}-{year+1}/results/"
-            )
-            
-            print(f"Extracting results for {year}-{year+1}...")
-            extract_results(url, filename)
-            time.sleep(5)
+    for folder, league_url in leagues.items():
+        archive = get_archive(league_url)
+        os.makedirs(folder, exist_ok=True)
+        for url in archive:
+            print(f"Extracting results for {url}...")
+            pattern = r"(\d{4}-\d{4}|\d{4})"
+            matches = re.findall(pattern, url)
+            year_str = matches[0]
+            filename = os.path.join(folder, f"{year_str}.csv")
+            if not os.path.exists(filename):
+                url = f"{MAIN_URL}{url}results/"
+                extract_results(url, filename)
+                time.sleep(5)
