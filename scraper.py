994e8f25f77112830d3b224843c85c0aec634d0f
Rodrigo Azevedo <rodrigo4zevedo@gmail.com>
2025-06-25 16:02:37 +0200

    Change scraper

--- scraper.py
+++ scraper.py
@@ -1,7 +1,9 @@
 import os
-import time
 import re
+import time
+from datetime import datetime
 
+import pytz
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
@@ -13,22 +15,43 @@ from selenium.webdriver.common.by import By
 leagues = {
     # "data/clubs/uruguay-primera-division": "/uruguay/liga-auf-uruguaya",
     # "data/clubs/argentina-primera-division": "/argentina/torneo-betano",
+    # "data/clubs/argentina-copa-argentina": "/argentina/copa-argentina",
+    "data/clubs/argentina-copa-de-la-liga": "/argentina/copa-de-la-liga-profesional",
     # "data/clubs/brazil-brasileirao-serie-a": "/brazil/serie-a-betano",
     # "data/clubs/brazil-copa-do-brasil": "/brazil/copa-betano-do-brasil",
     # "data/clubs/chile-primera-division": "/chile/liga-de-primera",
+    # "data/clubs/chile-copa-chile": "/chile/copa-chile",
     # "data/clubs/bolivia-primera-division": "/bolivia/division-profesional",
     # "data/clubs/colombia-primera-a": "/colombia/primera-a",
+    # "data/clubs/colombia-copa-colombia": "/colombia/copa-colombia",
     # "data/clubs/ecuador-liga-pro": "/ecuador/liga-pro",
     # "data/clubs/paraguay-copa-de-primera": "/paraguay/copa-de-primera",
     # "data/clubs/peru-liga-1": "/peru/liga-1",
     # "data/clubs/venezuela-liga-futve": "/venezuela/liga-futve",
     # "data/clubs/mexico-liga-mx": "/mexico/liga-mx",
-    "data/clubs/south-america-copa-libertadores": "/south-america/copa-libertadores",
-    "data/clubs/south-america-copa-sudamericana": "/south-america/copa-sudamericana",
-    "data/clubs/north-central-america-concacaf-champions-cup": "/north-central-america/concacaf-champions-cup",
-    "data/clubs/usa-mls": "/usa/mls",
+    # "data/clubs/south-america-copa-libertadores": "/south-america/copa-libertadores",
+    # "data/clubs/south-america-copa-sudamericana": "/south-america/copa-sudamericana",
+    # "data/clubs/north-central-america-concacaf-champions-cup": "/north-central-america/concacaf-champions-cup",
+    # "data/clubs/north-central-america-concacaf-caribbean-cup": "/north-central-america/concacaf-caribbean-cup",
+    # "data/clubs/north-central-america-concacaf-central-american-cup": "/north-central-america/concacaf-central-american-cup",
+    # "data/clubs/north-central-america-concacaf-caribbean-shield": "/north-central-america/concacaf-caribbean-shield",
+    # "data/clubs/dominican-republic/ldf": "/dominican-republic/ldf",
+    # "data/clubs/haiti/championnat-national": "/haiti/championnat-national",
+    # "data/clubs/jamaica/premier-league": "/jamaica/premier-league",
+    # "data/clubs/trinidad-and-tobago/tt-premier-league": "/trinidad-and-tobago/tt-premier-league",
+    # "data/clubs/costa-rica/primera-division": "/costa-rica/primera-division",
+    # "data/clubs/el-salvador/primera-division": "/el-salvador/primera-division",
+    # "data/clubs/guatemala/liga-nacional": "/guatemala/liga-nacional",
+    # "data/clubs/honduras/liga-nacional": "/honduras/liga-nacional",
+    # "data/clubs/panama/lpf": "/panama/lpf",
+    # "data/clubs/nicaragua/liga-primera": "/nicaragua/liga-primera",
+    # "data/clubs/usa-mls": "/usa/mls",
+    # "data/clubs/usa-us-open-cup": "/usa/us-open-cup",
+    # "data/clubs/canada-canadian-championship": "/canada/championship",
+    # "data/clubs/canada-canadian-premier-league": "/canada/canadian-premier-league",
 }
 
+TIMEZONE = "Europe/Madrid"
 MAIN_URL = "https://www.flashscore.com"
 
 
@@ -37,6 +60,7 @@ def download_logo(src, team_name):
         return
     folder = "logos"
     os.makedirs(folder, exist_ok=True)
+    team_name = re.sub(r"\(\w{3}\)", "", team_name).strip()
     team_name = team_name.replace(" ", "_").lower()
     filename = os.path.join(folder, f"{team_name}.png")
     if not os.path.exists(filename):
@@ -46,12 +70,70 @@ def download_logo(src, team_name):
                 f.write(response.content)
 
 
-def extract_results_from_html(html_content):
+def get_timestamp(date_str, year):
+    match = re.search(r"(\d{2}\.\d{2})\.\s+(\d{2}:\d{2})", date_str)
+    if match:
+        date = match.group(1)
+        time = match.group(2)
+        full_date_str = f"{date}.{year} {time}"
+        date_format = "%d.%m.%Y %H:%M"
+        naive_dt = datetime.strptime(full_date_str, date_format)
+        tz = pytz.timezone(TIMEZONE)
+        utc_tz = pytz.utc
+        dt = tz.localize(naive_dt)
+        utc_dt = dt.astimezone(utc_tz)
+        return int(utc_dt.timestamp())
+    match = re.search(r"(\d{2}\.\d{2})\.", date_str)
+    if match:
+        date = match.group(1)
+        full_date_str = f"{date}.{year}"
+        date_format = "%d.%m.%Y"
+        utc_dt = datetime.strptime(full_date_str, date_format)
+        return int(utc_dt.timestamp())
+
+
+def get_month(date_str):
+    match = re.search(r"\d{2}\.(\d{2})\.", date_str)
+    if match:
+        month = match.group(0)
+        return int(month)
+    return None
+
+
+def get_info(date_str):
+    match = re.search(r"([a-zA-Z]+)", date_str)
+    if match:
+        return match.group(1)
+    return ""
+
+
+def get_year(date_str):
+    match = re.search(r"\-(\d{4})\-(\d{4})\/", date_str)
+    if match:
+        return [int(match.group(1)), int(match.group(2))]
+    match = re.search(r"\-(\d{4})\/", date_str)
+    if match:
+        return int(match.group(1))
+
+
+def extract_results_from_html(html_content, year):
     soup = BeautifulSoup(html_content, "html.parser")
-    results = ["date,home_team,away_team,home_score,away_score"]
+    results = [
+        "timestamp,home_team,away_team,home_score,away_score,info,home_part,away_part"
+    ]
     matches = soup.find_all("div", class_=["event__match"])
-    for match in matches:
+    last_month = None
+    current_year = year if isinstance(year, int) else year[0]
+    for match in matches[::-1]:
         date = match.find("div", class_=["event__time"]).get_text(strip=True)
+        info = get_info(date)
+        if isinstance(year, list):
+            month = get_month(date)
+            if last_month and month < last_month:
+                current_year = year[1]
+            else:
+                last_month = month
+        timestamp = get_timestamp(date, current_year)
         home = match.find("div", class_=["event__homeParticipant"])
         away = match.find("div", class_=["event__awayParticipant"])
         home_score = match.find("span", class_=["event__score--home"]).get_text(
@@ -60,6 +142,18 @@ def extract_results_from_html(html_content):
         away_score = match.find("span", class_=["event__score--away"]).get_text(
             strip=True
         )
+        home_part = match.find("div", class_=["event__part--home"])
+        away_part = match.find("div", class_=["event__part--away"])
+        if home_part and away_part:
+            home_temp = home_score
+            away_temp = away_score
+            home_score = home_part.get_text(strip=True)
+            away_score = away_part.get_text(strip=True)
+            home_part = home_temp
+            away_part = away_temp
+        else:
+            home_part = ""
+            away_part = ""
         home_team = (home.find("span") or home.find("strong")).get_text(strip=True)
         away_team = (away.find("span") or away.find("strong")).get_text(strip=True)
         home_img = home.find("img")
@@ -68,7 +162,20 @@ def extract_results_from_html(html_content):
         away_logo = away_img["src"] if away_img and away_img.has_attr("src") else None
         download_logo(home_logo, home_team)
         download_logo(away_logo, away_team)
-        results.append(",".join([date, home_team, away_team, home_score, away_score]))
+        results.append(
+            ",".join(
+                [
+                    str(timestamp),
+                    home_team,
+                    away_team,
+                    home_score,
+                    away_score,
+                    info,
+                    home_part,
+                    away_part,
+                ]
+            )
+        )
     return results
 
 
@@ -106,7 +213,7 @@ def get_archive(url):
         time.sleep(2)
 
         source = driver.page_source
-        pattern = rf"(\/football{url}\-.*\/)"
+        pattern = rf"(\/football{url}\-.*\/)\""
         matches = re.findall(pattern, source)
         return matches
 
@@ -168,8 +275,11 @@ def extract_results(url, filename):
 
     except NoSuchElementException:
         time.sleep(5)
+        year = get_year(url)
         div_element = driver.find_element(By.ID, "live-table")
-        results = extract_results_from_html(div_element.get_attribute("innerHTML"))
+        results = extract_results_from_html(
+            div_element.get_attribute("innerHTML"), year
+        )
         with open(filename, "w") as f:
             f.write("\n".join(results))
     except TimeoutException:
@@ -185,12 +295,14 @@ if __name__ == "__main__":
         archive = get_archive(league_url)
         os.makedirs(folder, exist_ok=True)
         for url in archive:
-            print(f"Extracting results for {url}...")
-            pattern = r"(\d{4}-\d{4}|\d{4})"
+            pattern = r"-(\d{4}-\d{4}|\d{4})\/"
             matches = re.findall(pattern, url)
+            if not matches:
+                continue
             year_str = matches[0]
             filename = os.path.join(folder, f"{year_str}.csv")
             if not os.path.exists(filename):
+                print(f"Extracting results for {url}...")
                 url = f"{MAIN_URL}{url}results/"
                 extract_results(url, filename)
                 time.sleep(5)
