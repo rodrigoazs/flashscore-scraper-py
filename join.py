c5066cd66013bd5d69a9a99fedf300f3446e7e05
Rodrigo Azevedo <rodrigo4zevedo@gmail.com>
2025-07-04 09:17:12 +0200

    update europe matches

--- join.py
+++ join.py
@@ -2,6 +2,39 @@ import os
 
 import pandas as pd
 
+
+def get_place(path):
+    place = path.split("-")[0]
+    if place == "north":
+        if path.split("-")[1] == "central":
+            return "north and central america"
+        if path.split("-")[1] == "macedonia":
+            return "north macedonia"
+    if place == "south":
+        return "south america"
+    if place == "dominican":
+        return "dominican republic"
+    if place == "trinidad":
+        return "trinidad and tobago"
+    if place == "costa":
+        return "costa rica"
+    if place == "el":
+        return "el salvador"
+    if place == "bosnia":
+        return "bosnia and herzegovina"
+    if place == "czech":
+        return "czech republic"
+    if place == "faroe":
+        return "faroe islands"
+    if place == "northern":
+        return "northern ireland"
+    if place == "san":
+        return "san marino"
+    if place == "usa":
+        return "united states"
+    return place
+
+
 if __name__ == "__main__":
     folder_path = "data"
     all_files = []
@@ -29,7 +62,7 @@ if __name__ == "__main__":
     )
     for file in all_files:
         print(f"Loading {file}")
-        place = file.split("/")[2].split("-")[0]
+        place = get_place(file.split("/")[2])
         season = file.split("/")[3].split(".")[0]
         tournament = file.split("/")[2]
         load_df = pd.read_csv(file)
